39755cd31d7937d45e6fc850cabc836b4d1c020f
Jonas Lalin <1ac6df24@gmail.com>
2019-07-31 09:26:46 +0200

    read files using __dirname

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Document.location.href Behaviour
 
-This project demonstrates how outstanding requests are cancelled, in a scenario where the user-agent has been given a redirect command.
+This project demonstrates when and how outstanding requests are aborted, in a scenario where the user-agent has been given a redirect command.
 
 Be sure to check out how Internet Explorer 11 differs from Google Chrome.
 
--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-const http = require('http');
 const fs = require('fs');
-
+const http = require('http');
+const path = require('path');
 const port = process.env.SERVER_PORT || 3000;
 
 function requestHandler(req, res) {
   const url = new URL(req.url, `http://localhost:${port}/`);
-  const path = url.pathname;
+  const pathname = url.pathname;
   const delay = url.searchParams.get('delay') || 0;
-  console.log(`serving path ${path}, with delay ${delay} ms`);
+  console.log(`serving path ${pathname}, with delay ${delay} ms`);
 
   setTimeout(() => {
-    switch (path) {
+    switch (pathname) {
       case '/':
-      case '/redirect':
+      case '/redirect': {
+        const file = path.resolve(
+          __dirname,
+          `${pathname.substr(1) || 'index'}.html`
+        );
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.createReadStream((path.substr(1) || 'index') + '.html').pipe(res);
+        fs.createReadStream(file).pipe(res);
         break;
+      }
       case '/red.gif':
-      case '/blue.gif':
+      case '/blue.gif': {
+        const file = path.resolve(__dirname, pathname.substr(1));
         res.writeHead(200, { 'Content-Type': 'image/gif' });
-        fs.createReadStream(path.substr(1)).pipe(res);
+        fs.createReadStream(file).pipe(res);
         break;
+      }
       case '/hello.js':
-      case '/goodbye.js':
+      case '/goodbye.js': {
+        const file = path.resolve(__dirname, pathname.substr(1));
         res.writeHead(200, { 'Content-Type': 'text/javascript' });
-        fs.createReadStream(path.substr(1)).pipe(res);
+        fs.createReadStream(file).pipe(res);
         break;
+      }
       default:
         res.writeHead(404);
         res.end();
